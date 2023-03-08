15105d035668600f7646ecda9eb956e792188cef
BrunoPrimi <brunoreballo@gmail.com>
2023-03-08 08:42:38 -0300

    Aula sobre javascript cpm variaveis com Funcoes

---
+++ aula02/funcoes.js
@@ -0,0 +1,24 @@
+let num1 = 2
+const num2 = 3
+
+function soma()
+{
+
+return num1 + num2
+
+}
+
+console.log(soma())
+
+num1 = 6
+// num2 = 9
+
+console.log(soma())
+
+const funcaoSoma = (a, b)=>{
+
+    return a + b
+
+}
+
+console.log(funcaoSoma(10, 20))
---
+++ aula02/funcoesevariaveis.js
@@ -0,0 +1,14 @@
+var variavel = "Olá"
+let variavel2 = 3
+const variavel3 = 6
+
+console.log(variavel)
+console.log(variavel2 + variavel3)
+
+const obj ={
+    nome:"Bruno",
+    signo:"Sagitario"
+
+}
+console.log(obj)
+console.log(typeof obj)
