fb50fd91b3b706c224db90fd103d8bc137e4f3cc
BrunoPrimi <brunoreballo@gmail.com>
2023-04-12 18:56:39 -0300

    Exercicios de operacao de matematica

---
+++ Exercicios/Operacao_matematica.js
@@ -0,0 +1,47 @@
+console.log("Adição")
+
+const soma =(a,b)=>{
+    return a+b
+}
+
+console.log("10 + 10 =",soma(10,10))
+
+console.log("\nSubtração")
+
+const sub =(a,b)=>{
+    return a-b
+}
+
+console.log("15 - 10 =",sub(15,10))
+
+console.log("\nMultiplicação")
+
+const mult =(a,b)=>{
+    return a*b
+}
+
+console.log("10 x 10 =",mult(10,10))
+
+console.log("\nDivisão")
+
+const div =(a,b)=>{
+    return a/b
+}
+
+console.log("100 / 10 =",soma(100,10))
+
+console.log("\nPotenciação")
+
+const pot =(a,b)=>{
+    return a**b
+}
+
+console.log("10² =",pot(10,2))
+
+console.log("\nRaiz")
+
+const raiz =(a)=>{
+    return Math.sqrt(a)
+}
+
+console.log("√100",raiz(100))
